f5e69a2754eaeed36978f509a2bd1dc105cbb77d
nicocortese <nicolascortese2004@gmail.com>
2026-04-03 15:29:04 -0300

    force cors middleware

--- app.js
+++ app.js
@@ -22,14 +22,17 @@ app.set("port", process.env.PORT || 4000);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: [
-      "https://retro-kicks.vercel.app",
-    ],
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 /* Routes */
 app.use("/", indexRoutes);
